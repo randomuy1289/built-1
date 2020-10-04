7e8495771d207e138c62b8839e578c2ce24b4237
randomguy1289 <ashutoshshenoy@gmail.com>
2020-10-04 16:43:58 +0530

    Add function
    Made a c function to add two numbers

--- working.c
+++ working.c
@@ -5,7 +5,10 @@ int addFunc(int a, int b){
 	return a + b;
 }
 int main( int argc, char ** argv ) {
+	int a,b;
 	printf("Hello, World!\n");
-	printf("The result of the addition is %d" \n,addFunc(9,10));
+	printf("Please enter the two numbers \n");
+	scanf("%d%d \n",&a,&b);
+	printf("The result of the addition is %d" \n,addFunc(a,b));
 	return 0;
 }
